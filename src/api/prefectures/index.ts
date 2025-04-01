c0a6c14985451c63c32fae99e30c80ee0a5ae524
eretica <eretica.m@gmail.com>
2025-04-01 20:57:37 +0900

    都道府県を返すAPIの追加

---
+++ src/api/prefectures/index.ts
@@ -0,0 +1,2 @@
+export { usePrefectures } from "./prefectures";
+export type { Prefecture, PrefecturesResponse } from "./prefectures";
---
+++ src/api/prefectures/prefectures.ts
@@ -0,0 +1,19 @@
+import { get } from "../client";
+import { useQuery } from "@tanstack/react-query";
+
+export type Prefecture = {
+  prefCode: number;
+  prefName: string;
+};
+
+export type PrefecturesResponse = {
+  message: string;
+  result: Prefecture[];
+};
+
+export const usePrefectures = () => {
+  return useQuery<PrefecturesResponse>({
+    queryKey: ["prefectures"],
+    queryFn: () => get("/api/v1/prefectures"),
+  });
+};
