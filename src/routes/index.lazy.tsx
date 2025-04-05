0efc8fd966d869a7a9af0c7486dd2dc2d4ebe69f
eretica <eretica.m@gmail.com>
2025-04-06 00:11:57 +0900

    トップ画面のレイアウトを反映

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,7 +1,8 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { useIndexPage } from "@/routes/-hooks/index.page.hooks.ts";
-import { PrefectureCheckboxContainer } from "@/routes/-components/PrefectureCheckbox";
-import { PopulationCompositionPerYearGraphContainer } from "@/routes/-components/PopulationCompositionPerYearGraph";
+import { PrefectureCheckboxContainer } from "./-components/PrefectureCheckbox";
+import { PopulationCompositionPerYearGraphContainer } from "./-components/PopulationCompositionPerYearGraph";
+import styles from "./-styles/index.module.scss";
 
 export const Route = createLazyFileRoute("/")({
   component: IndexLazy,
@@ -11,7 +12,8 @@ function IndexLazy() {
   const { selectedPrefectures, setSelectedPrefectures } = useIndexPage();
 
   return (
-    <>
+    <div className={styles.container}>
+      <div className={styles.prefecturesHeading}>都道府県選択</div>
       <PrefectureCheckboxContainer
         values={selectedPrefectures}
         onChange={(values) => {
@@ -19,11 +21,17 @@ function IndexLazy() {
         }}
       />
 
+      {!selectedPrefectures.length && (
+        <div className={styles.hintBox}>
+          都道府県を選択するとグラフが表示されます
+        </div>
+      )}
+
       {!!selectedPrefectures.length && (
         <PopulationCompositionPerYearGraphContainer
           prefCodes={selectedPrefectures}
         />
       )}
-    </>
+    </div>
   );
 }
