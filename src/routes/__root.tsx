17719fd7fe050312d66d71a796c1a679def18a4b
eretica <eretica.m@gmail.com>
2025-04-07 23:56:39 +0900

    fix import

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import { AppHeader } from "@/components/AppHeader/Appheader.tsx";
+import { AppHeader } from "@/components/AppHeader";
 
 export const Route = createRootRoute({
   component: () => (
