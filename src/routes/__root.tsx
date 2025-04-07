2ab1d33676da89e3611058331d7a59388abc0772
eretica <eretica.m@gmail.com>
2025-04-07 22:45:17 +0900

    サービスのヘッダーを追加

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { AppHeader } from "@/components/AppHeader/Appheader.tsx";
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <AppHeader />
       <Outlet />
       <TanStackRouterDevtools />
     </>
